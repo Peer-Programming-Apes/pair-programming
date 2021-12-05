7454eb837494021c894cd6d2c9a4f37eaa6b75a0
Subodh Singh <singh.01.subodh@gmail.com>
2021-12-05 21:09:25 +0530

    next.js ta ke shorao

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,59 +1,43 @@
 import Image from "next/image";
 import Layout from "../components/Layout.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import Cookies from "js-cookie";
 import { addUser, removeUser } from "../store/userSlice";
 import { addToken, removeToken } from "../store/authTokenSlice";
 import styles from "../styles/Home.module.css";
 import { Button, Form } from "react-bootstrap";
 
-export async function getServerSideProps(context) {
-	console.log("inside getserverside props");
-	// try {
-	//   console.log("fetching data");
-	// 	const res = await axios.get("http://localhost:4000/profile", {
-	// 		withCredentials: true,
-	// 	});
-	//   console.log("data fetched succesfully", res);
 
-	// 	return { props: { user: res.data.user } };
-	// } catch (e) {
-	//   console.log("failed inside getserverside props");
-	// 	console.log(e.response.status);
-
-	// 	return { props: {} };
-	// }
-
-	// Fetch data from external API
-	const res = await fetch("http://localhost:4000/profile", {
-		credentials: "include",
-	});
-  console.log(res.ok);
-	const data = await res.json();
-
-	if (!res.ok) {
-		console.log("not data serverside");
-		return {
-			props: {},
-		};
-	}
-
-	// Pass data to the page via props
-	console.log("data received serverside");
-	return { props: { user: data.user } };
-}
-
-const Home = (props) => {
+const Home = () => {
+	// const [ready, setReady] = useState(false);	// for other pages
 	const dispatch = useDispatch();
 	const isLoggedIn = useSelector((state) => state.auth.token);
 
-	useEffect(() => {
+	useEffect(async () => {
 		console.log("inside useeffect");
-		if (props.user) {
+
+		// Fetch data from external API
+		const res = await fetch("http://localhost:4000/profile", {
+			credentials: "include",
+		});
+
+		console.log(res.ok);
+
+		if (!res.ok) {
+			console.log("Server !ok");
+			// redirect kore de home page e na thakle
+			return;
+		}
+
+		const data = await res.json();
+		// Pass data to the page via props
+		console.log("data received serverside");
+
+
+
+		if (data.user) {
 			console.log("user received");
-			const { _id, email, name, googleID, picture } = props.user;
+			const { _id, email, name, googleID, picture } = data.user;
 			const userPayload = { _id, email, name, googleID, picture };
 			const TOKEN = JSON.stringify(userPayload); //for local storage
 			console.log(userPayload);
@@ -61,55 +45,16 @@ const Home = (props) => {
 			//adding user to user state in redux store
 			dispatch(addUser(userPayload));
 			dispatch(addToken(TOKEN));
+
+			// setReady(true);	// for other pages
 		} else {
 			console.log("user not received");
 			dispatch(removeToken());
 			dispatch(removeUser());
 		}
+
 	}, []);
 
-	//both login and logout is handled here as both the times we are redirected to this page
-	// useEffect(() => {
-	// 	(async () => {
-	// 		try {
-	// 			// const res = await axios.get("http://localhost:4000/profile", {
-	// 			// 	withCredentials: true,
-	// 			// });
-	// 			console.log("inside use effect");
-	// 			const res = await fetch("http://localhost:4000/profile", {
-	// 				credentials: "include",
-	// 			});
-	// 			const data = await res.json();
-	// 			console.log(data);
-
-	// 			// return userData;
-	// 			const user = data.user;
-	// 			const { _id, email, name, googleID, picture } = user;
-	// 			const userPayload = { _id, email, name, googleID, picture };
-	// 			const TOKEN = JSON.stringify(userPayload); //for local storage
-	// 			console.log(userPayload);
-
-	// 			//adding user to user state in redux store
-	// 			dispatch(addUser(userPayload));
-	// 			dispatch(addToken(TOKEN));
-
-	// 			//add user to local storage
-	// 			localStorage.setItem("authUserInfo", TOKEN);
-
-	// 			//add a cookie
-	// 			Cookies.set("nextAuthCookie", TOKEN, { sameSite: "strict" });
-	// 		} catch (e) {
-	// 			// console.log(e.response.status);
-	// 			dispatch(removeToken());
-	// 			localStorage.removeItem("authUserInfo");
-	// 			dispatch(removeUser());
-	// 			console.log(e);
-
-	// 			//remove cookie
-	// 			Cookies.remove("nextAuthCookie");
-	// 		}
-	// 	})();
-	// }, []);
 
 	const createLinkHandler = (e) => {
 		e.preventDefault();
