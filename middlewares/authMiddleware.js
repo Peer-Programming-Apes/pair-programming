e8b1312076eeae53d05f335568172ce494d90b97
Sagnik Haldar <sagnikh2@gmail.com>
2021-12-30 00:41:43 +0530

    added logs in server authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,10 @@
 const checkUser = (req, res, next) => {
+	console.log("entering middleware");
 	if (req.user) {
-		// console.log("req.user");
+		console.log("middleware success : ", req.user);
 		next();
 	} else {
+		console.log("middleware failure");
 		res.status(403).json("unauthorized");
 	}
 };
