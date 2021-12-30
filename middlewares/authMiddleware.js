e7f1f01b6a14bb2a2defdf0255345e3c742e4053
Sagnik Haldar <sagnikh2@gmail.com>
2021-12-30 21:55:21 +0530

    fixed authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,9 +4,9 @@ const checkUser = (req, res, next) => {
 		console.log("middleware success : ", req.user);
 		next();
 	} else {
-		// console.log("middleware failure");
-		// res.status(403).json("unauthorized");
-    next();
+		console.log("middleware failure");
+		res.status(403).json("unauthorized");
+    // next();
 	}
 };
 
